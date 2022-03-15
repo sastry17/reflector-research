24d4ef305a4354937aa9d6f50d82b5a9493b7374
sastry17 <shreyas.srinivasa@outlook.com>
2022-03-15 09:24:11 -0700

    added nmap script for all the IPs

--- megaDetector.py
+++ megaDetector.py
@@ -1,33 +1,75 @@
 from asyncore import read
 import pandas as pd
 import argparse
+from scapy.all import *
+import csv
+import subprocess
 
-def filter_data(filename):
-    file1 = open(filename, 'r')
-    count =0
-    ldapfiles = file1.readlines()
-    for line in ldapfiles:
-        line = line.strip()
-        count+=1
-        input_data = pd.read_csv(line,compression="gzip",sep=",", names=["saddr","sport","daddr","dport","classification","icmp_unreach_str","icmp_code","icmp_type","icmp_responder","success"], usecols=['saddr','sport','classification'])
-        df=input_data.loc[input_data['classification']=="udp"]
-        print("udp filter done")
-        #df.drop_duplicates("saddr",keep=False, inplace=True)
-        #identify the duplicates and their count
-        #df=df.groupby(df.columns.tolist(),as_index=False).size()
-        #df=input_data.loc[input_data['saddr']!="202.232.184.169"]
-        udf=df[['saddr','sport']]
-        udf=udf.groupby(udf.columns.tolist(),as_index=False).size()
-        print("File loaded to dataframe")
-        #print(udf)
-        newfile=str(count)+line[30:-4]+'-ntp-mega-hosts.txt'
-        udf.to_csv(newfile, header=None, index=None,  mode='a')
-        print(newfile)
-        print("Done!")
+
+
+
+def ungzip(filename):
+    subprocess.run()
+
+
+    count_amps(pcapfile)
+    subprocess.run("[gzip]", "[-d]", filename)
+    pcapfile = 
+
+
+
+
+
+def count_amps(pcapfile):
+    print("Reading pcap")
+    packets = PcapReader(pcapfile)
+    print("Completed reading pcap")
+    newfile=+pcapfile[30:-4]+'-NTP-Mega-hosts.txt'
+    df = pd.DataFrame()
+
+
+
+    for packet in packets:
+        if packet.haslayer("NTP") and packet[2].sport==123 and packet[1].dst=='':
+            src = packet[1].src
+            dst = packet[1].dst
+            ttl = packet[1].ttl
+            sport = packet[2].sport  
+            precision = packet[3].precision
+            delay= packet[3].delay
+            dispersion = packet[3].dispersion
+            orig = packet[3].orig
+            packet.show()
+        #print(src, sport, ver)
+            row = "\n"+src+', '+str(sport)+', '+dst+', '+str(precision)+', '+str(delay)+', '+str(dispersion)+', '+str(orig)
+            df.insert(row)
+            df=df.groupby(df.columns.tolist(),as_index=False).size()
+            df.to_csv(newfile, header=None, index=None,  mode='a')
+        #print(row)
+        #newfile.write(row)
+        #count += 1
+        #print(count)
+    newfile.close()
+    print("Done!")
+
+#pkt=packets[4]
+#print(dir(pkt))
+#?print(pkt.show())
+
+#print(pkt[NTP].srcaddr)
+#print(pkt[3].version)
+
+
+
+
+#count = 0
+
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", required=True)
 args = parser.parse_args()
 filename=args.file
 print(filename)
-filter_data(filename)
+ungzip(filename)
---
+++ nmapip.py
@@ -0,0 +1,62 @@
+from cmath import e
+from xdrlib import Error
+import nmap
+import argparse
+
+
+
+def scan(file):
+    f = open(file, 'r')
+    s = open("nmapOut.csv",'w')
+    hostsl = f.readlines()
+    for line in hostsl:
+        try:
+            line=line.strip()
+            nm = nmap.PortScanner()
+            nm.scan(hosts=line, arguments='-sU -sT')
+            print(line)
+            
+            #print(nm[line].state())           
+            #print(nm.scaninfo())
+            #print(isinstance(nm.all_hosts(),bool))
+            
+            for ip in nm.all_hosts():
+                #print(ip)
+                #print('----------------------------------------------------')
+                #print('Host : %s (%s)' % (host, nm[host].hostname()))
+                #print('State : %s' % nm[host].state())
+                
+                for proto in nm[ip].all_protocols():
+                    try:
+                        #print('----------')
+                        #print('Protocol : %s' % proto)
+                        lport = nm[ip][proto].keys()
+                        
+                        #print(lport.csv)
+                        #lport.sort()
+                        for port in lport:
+                            #print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+                            pname=nm[ip][proto][port]['name']
+                            #row =  str("\n"+host+", "+proto+", "+str(port)+", "+pname)
+                            row =  "\n"+ip+", "+str(port)+", "+proto+", "+pname
+                            s.write(row)
+                            print(row)
+                    except Error as e:
+                        print(e)
+                        pass
+        except Error as e:
+            print(e)
+            pass
+
+    s.close()
+    
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", required=True)
+args = parser.parse_args()
+filename=args.file
+print(filename)
+scan(filename)
